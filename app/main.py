578ab01476a3196a556738cd8d3164a6e9246adb
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-22 15:03:42 +0530

    chore: remove templates render

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-# from fastapi.staticfiles import StaticFiles
 
 from app.internal import admin
 from app.routers import login
 
 app = FastAPI()
-# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(login.router)
 app.include_router(admin.router, prefix="/admin")
--- app/routers/login.py
+++ app/routers/login.py
@@ -1,11 +1,8 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import APIRouter
 
 router = APIRouter()
-templates = Jinja2Templates(directory="app/templates")
 
 
-@router.get("/login", response_class=HTMLResponse)
-def login(request: Request):
-    return templates.TemplateResponse(request=request, name="login.html")
+@router.get("/login")
+def login():
+    return {"message": "/login"}
