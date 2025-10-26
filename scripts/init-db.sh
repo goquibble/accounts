f569ea62f5eb09223bbe785f0c1c13ce191bf51a
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-26 23:52:02 +0530

    chore: add set -ex to init_scripts

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+set -ex
 # create database (ignore error if already exists)
 psql -h db -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE $POSTGRES_DB;" || true
--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 set -ex
-
 # run migrations
 uv run alembic upgrade head
 # create initial data in DB
