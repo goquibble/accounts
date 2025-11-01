33105885c4f1692926d1247525db26e8d7e026d4
stabldev <114811070+stabldev@users.noreply.github.com>
2025-11-01 22:34:07 +0530

    chore: remove help link from header comp.

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,25 +1,15 @@
 export default function Header() {
   return (
-    <header className="fixed top-0 inset-x-0 p-3 flex items-center justify-between">
+    <header className="fixed top-0 inset-x-0 p-3">
       <a
         href="https://github.com/goquibble"
         target="_blank"
         rel="noreferrer"
-        className="flex items-center gap-2"
+        className="flex items-center gap-2 w-max"
       >
         <img src="/favicon.svg" alt="Quibble" className="size-6" />
         <span className="font-medium">GoQuibble Platform</span>
       </a>
-      <div className="flex items-center gap-4">
-        <a
-          href="https://github.com/orgs/goquibble/discussions/new?category=q-a"
-          target="_blank"
-          rel="noreferrer"
-          className="text-primary underline"
-        >
-          Need help?
-        </a>
-      </div>
     </header>
   );
 }
