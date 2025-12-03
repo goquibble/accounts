4073cfcf8b9ab89868b1ed5d1379b9cc84a1a344
stabldev <114811070+stabldev@users.noreply.github.com>
2025-12-03 20:43:36 +0530

    chore: update init-db script to use POSTGRES_PASSWORD

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -ex
+
+export PGPASSWORD="$POSTGRES_PASSWORD"
 # create database (ignore error if already exists)
 psql -h db -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE $POSTGRES_DB;" || true
