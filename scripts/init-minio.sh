2a06a85d87921b305af22f127720ab7fc5bbba24
stabldev <114811070+stabldev@users.noreply.github.com>
2026-01-13 17:01:22 +0530

    chore: remove minio init script

--- scripts/init-minio.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -ex
-# script to run inside container to make minIO bucket public
-mc alias set local http://minio:9000 "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY"
-mc mb -p local/"$AWS_S3_BUCKET_NAME" || true
-mc anonymous set public local/"$AWS_S3_BUCKET_NAME"
