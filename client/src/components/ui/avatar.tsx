903a86ccef8aa81ff555cdbc861d9c5530db32c4
stabldev <114811070+stabldev@users.noreply.github.com>
2026-01-21 18:07:18 +0530

    fix: add object cover to avatar preview

--- client/src/components/ui/avatar.tsx
+++ client/src/components/ui/avatar.tsx
@@ -4,52 +4,55 @@ import type * as React from "react";
 import { cn, getAvatarUrl } from "@/lib/utils";
 
 function Avatar({
-  className,
-  ...props
+	className,
+	...props
 }: React.ComponentProps<typeof AvatarPrimitive.Root>) {
-  return (
-    <AvatarPrimitive.Root
-      data-slot="avatar"
-      className={cn("flex size-8 shrink-0", className)}
-      {...props}
-    />
-  );
+	return (
+		<AvatarPrimitive.Root
+			data-slot="avatar"
+			className={cn("flex size-8 shrink-0", className)}
+			{...props}
+		/>
+	);
 }
 
 function AvatarImage({
-  className,
-  ...props
+	className,
+	...props
 }: React.ComponentProps<typeof AvatarPrimitive.Image>) {
-  return (
-    <AvatarPrimitive.Image
-      data-slot="avatar-image"
-      className={cn("aspect-square size-full rounded-full", className)}
-      {...props}
-    />
-  );
+	return (
+		<AvatarPrimitive.Image
+			data-slot="avatar-image"
+			className={cn(
+				"aspect-square size-full rounded-full object-cover",
+				className,
+			)}
+			{...props}
+		/>
+	);
 }
 
 function AvatarFallback({
-  className,
-  seed,
-  format,
-  size,
-  ...props
+	className,
+	seed,
+	format,
+	size,
+	...props
 }: React.ComponentProps<typeof AvatarPrimitive.Fallback> & {
-  seed?: string;
-  format?: "svg" | "png";
-  size?: number;
+	seed?: string;
+	format?: "svg" | "png";
+	size?: number;
 }) {
-  const avatarUrl = getAvatarUrl(seed, format, size);
-  return (
-    <AvatarPrimitive.Fallback
-      data-slot="avatar-fallback"
-      className={cn("flex size-full", className)}
-      {...props}
-    >
-      <img src={avatarUrl} alt={`avatar-${seed}`} />
-    </AvatarPrimitive.Fallback>
-  );
+	const avatarUrl = getAvatarUrl(seed, format, size);
+	return (
+		<AvatarPrimitive.Fallback
+			data-slot="avatar-fallback"
+			className={cn("flex size-full", className)}
+			{...props}
+		>
+			<img src={avatarUrl} alt={`avatar-${seed}`} />
+		</AvatarPrimitive.Fallback>
+	);
 }
 
 export { Avatar, AvatarImage, AvatarFallback };
